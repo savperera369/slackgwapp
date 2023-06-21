afd5adda1d7dc4e18b6a0f55a53ac7c8e5ab159d
savperera369 <saviru2002@outlook.com>
2023-06-21 12:44:30 -0400

    become a member page complete

--- src/pages/BecomeAMember.js
+++ src/pages/BecomeAMember.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import '../index.css';
-import { Typography, Container, Card, CardMedia, CardContent} from '@mui/material';
+import slackForm from '../pdfs/SLACGKW_MEMBERSHIP_FORM.pdf';
+import { Typography, Container, Card, CardContent, Link} from '@mui/material';
 
 function BecomeAMember(){
     return (
         <Container className="topDoc">
             <Typography variant="h4">Become A Member</Typography>
+            <Card sx={{margin: 2, backgroundColor:"#FCF6F5"}} variant="outlined">
+                <CardContent>
+                    <Link variant="body1" href={slackForm} target="_blank" rel="noreferrer">Membership Form</Link>
+                </CardContent>
+            </Card>
         </Container>
     );
 }
